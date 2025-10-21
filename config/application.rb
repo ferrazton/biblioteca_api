06f5a3211485bf5b3edfce4d467c63c9ad70db8e
Antonio Ferraz <antonioaferraz1@gmail.com>
2025-10-21 18:54:08 -0300

    chore(auth): enable session middleware and configure Devise JWT for API mode

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,7 @@ module BibliotecaApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: "_biblioteca_api_session"
   end
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "_biblioteca_api_session"
